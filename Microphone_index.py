0542a8448a961027bbfbfed40be702e8c8adc68b
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-17 22:51:02 +0300

    -

--- Microphone_index.py
+++ Microphone_index.py
@@ -2,5 +2,3 @@
 import speech_recognition as sr
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print('Micropgone with name \"{1}\" found for `Microphone(device_index={0})`'.format(index, name))
-
-
