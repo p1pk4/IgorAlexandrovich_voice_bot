b0ca63f6794f5763aed651bbd2537539165434d0
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-17 22:33:58 +0300

    Добавлен код на определение индекса микрофона

---
+++ Microphone_index.py
@@ -0,0 +1,6 @@
+#Определение индекса нужного микрофона
+import speech_recognition as sr
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print('Micropgone with name \"{1}\" found for `Microphone(device_index={0})`'.format(index, name))
+
+
